3320bc459bbd2baa617c1cf8acf064b181a6c450
daniel ficci <danielficci@gmail.com>
2022-02-01 10:34:14 -0600

    added comments in js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict"
+// initializing variables
 const divBody = document.querySelector('#coffees');
 const roastSelection = document.querySelector('#roast-options');
 const addCoffeeSelection = document.querySelector('#roast-selection-add');
@@ -8,25 +9,28 @@ const searchBtn = document.querySelector('#search-coffee');
 const addCoffeeBtn = document.querySelector('#add-submit');
 const storedCoffee = JSON.parse(localStorage.getItem('listOfCoffee'));
 
-
+// jquery to give transition of fading in and our when switching between add your own coffee and browse our list
 $(document).ready(function () {
     $('#switch-modal').click(function () {
-        $('#modal-one').fadeOut(500)
-        $('#modal-two').fadeIn(2000)
+        $('#modal-one').fadeOut(500);
+        $('#modal-two').fadeIn(2000);
     })
-    $('#back').click(function (){
+    $('#back').click(function () {
         $('#modal-two').fadeOut(500);
         $('#modal-one').fadeIn(3000);
     })
-})
+});
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+// initializing coffee array
 let coffees = []
 
+// determining local stored data within your browser
 function getStoredData() {
-    if(storedCoffee){
+    // if not undefined use local storage
+    if (storedCoffee) {
         coffees = storedCoffee
-    } else if(!storedCoffee) {
+    } else if (!storedCoffee) {              // if no local storage use this array of objects
         coffees = [
             {id: 1, name: 'Light City', roast: 'light'},
             {id: 2, name: 'Half City', roast: 'light'},
@@ -45,7 +49,10 @@ function getStoredData() {
         ];
     }
 }
-getStoredData()
+
+getStoredData();
+
+// setting html layout and assigning value
 function renderCoffee(coffee) {
     let html = '<div class="coffee">';
     html += '<h2>' + coffee.name + '</h2>';
@@ -55,16 +62,30 @@ function renderCoffee(coffee) {
     return html;
 }
 
+// sorting and looping through an array of objects to display proper roast when clicked
 function renderCoffees(coffees) {
-    let coffeeCopy = coffees.slice();
-    let sortedCoffees = customSort({data: coffeeCopy, sortField: 'roast'})
+    let coffeeCopy = coffees.slice(); //making copy of original array
+    let sortedCoffees = customSort({data: coffeeCopy, sortField: 'roast'}) //sorting with custom sort
     let html = '';
-    for (let i = 0; i < sortedCoffees.length; i++) {
+    for (let i = 0; i < sortedCoffees.length; i++) { // looping through and assigning each coffee object
         html += renderCoffee(sortedCoffees[i]);
     }
     return html;
 }
 
+// sorts by their type of roasts
+const customSort = ({data, sortField}) => {
+    const sortBy = ['light', 'medium', 'dark'] //setting sort parameters
+    const sortByObject = sortBy.reduce((obj, item, index) => {
+        return { // returns obj sorted by parameters
+            ...obj,
+            [item]: index
+        }
+    }, {})
+    return data.sort((a, b) => sortByObject[a[sortField]] - sortByObject[b[sortField]]) //returning sort
+}
+
+// to update the ui
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     let selectedRoast = roastSelection.value.toLowerCase().split(' ');
@@ -82,6 +103,7 @@ function updateCoffees(e) {
     }
 }
 
+// search through  coffee list
 function searchCoffeeName() {
     const searchInputName = searchInput.value
     const searchNormalized = searchInputName.charAt(0).toUpperCase() + searchInputName.slice(1);
@@ -94,6 +116,7 @@ function searchCoffeeName() {
     divBody.innerHTML = renderCoffees(searchCoffees);
 }
 
+// adds coffee to the list and alerts user if no input is in the add coffee search
 const addCoffee = function (e) {
     e.preventDefault();
     // Getting roast selection
@@ -101,7 +124,7 @@ const addCoffee = function (e) {
     // Getting the value from add-coffee-input
     let coffeeInput = addCoffeeInput.value.split(' ');
     console.log(coffeeInput)
-    if(coffeeInput[0] !== ''){
+    if (coffeeInput[0] !== '') {
         let coffeeList = [];
         // turning the beginning of each word uppercase
         coffeeInput.forEach(function (coffee) {
@@ -122,16 +145,6 @@ const addCoffee = function (e) {
     }
 }
 
-const customSort = ({data, sortField}) => {
-    const sortBy = ['light', 'medium', 'dark']
-    const sortByObject = sortBy.reduce((obj, item, index) => {
-        return {
-            ...obj,
-            [item]: index
-        }
-    }, {})
-    return data.sort((a, b) => sortByObject[a[sortField]] - sortByObject[b[sortField]])
-}
 
 divBody.innerHTML = renderCoffees(coffees);
 
